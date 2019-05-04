605001f965d1652754e8327db7dcadabb28c42e3
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-05-04 14:56:09 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -10,27 +10,33 @@ string calculateBullAndPgia(string choise, string guess){
 		unsigned int i, j;
 		
 		bull=0;
+	i=0;
 		//while loop calculates how many bull-s there are and then erase them so they would not be calculated twice.
-		for (i=0; i < choise.length(); i++ ){ 
+		while(i < choise.length()){ 
 			if(choise[i]==guess[i]){
 				bull++;
 				choise.erase (i, 1);
 				guess.erase (i, 1);
 			}
+			else
+				++i;
 		}
 		
 		
 		pgia=0;
 		//two for loops calculate how many pgia-s there are and then erase them.
-		for( i=0; i<choise.length(); i++){
-			for(j=0; j<guess.length() && i<choise.length(); j++){
+		while (i<choise.length()){
+			while( j<guess.length() && i<choise.length()){
 				if(choise[i]==guess[j]){
 					pgia++;
 					choise.erase (i, 1);
 					guess.erase (j, 1);
-					--i; --j;
 				}
+				else
+					++j;
+				
 			}
+			++i;
 		}
 		
 		return std::to_string(bull)+","+std::to_string(pgia);

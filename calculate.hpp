ae69ab96514186cf3419b7aad839ac2fb06498f4
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-05-05 13:04:19 +0300

    Update calculate.hpp

--- calculate.hpp
+++ calculate.hpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 /**
+a classless function.
 calculate contains only one function.
 calculateBullAndPgia receives two strings choise and guess.
-the function calculates how many bull-s and pgia-s there are and returns a pair instance containing the results.
+the function calculates how many bull-s and pgia-s there are and returns a string instance containing the results.
 **/
 
 string calculateBullAndPgia(string choise, string guess);
